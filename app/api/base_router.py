9ddc977c713d3b985841de487c0cd8978ea21c78
reof1922 <reof1922@gmail.com>
2026-03-27 21:28:45 -0400

    IA para portal

--- app/api/base_router.py
+++ app/api/base_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .document_analyze.doc_analyze_router import doc_analyze_router
+from .public_pqrsd.router import pqrsd_router
 
 base_router = APIRouter(
     prefix="/api",
@@ -8,6 +9,7 @@ base_router = APIRouter(
 
 routers = [
     doc_analyze_router,
+    pqrsd_router,
 ]
 
 for router in routers:
